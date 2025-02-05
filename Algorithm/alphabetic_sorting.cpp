31cb9160286adae8ece8a8d481dc3f6dcb23fdec
Ahad Ali <ranaahad9095@gmail.com>
2025-02-04 22:39:23 -0800

    Practicing , Making an Algorithm to sort alphabets

---
+++ Algorithm/alphabetic_sorting.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSorting(char ar[], int size){
+    for (int i = 0; i < size-1; i++)
+    {
+        for (int j = 0; j < size-1; j++)
+        {
+            if (ar[j] > ar[j+1])
+            {
+                char temp = ar[j+1];
+                ar[j+1] = ar[j];
+                ar[j] = temp;
+            }    
+        }
+    }
+}
+
+void display(char arr[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    char ar[7] = {'e','f','z','u','d','m','a'};
+    int size = sizeof(ar)/sizeof(ar[0]);
+    bubbleSorting(ar,size);
+    display(ar,size);
+    
+    return 0;
+}
