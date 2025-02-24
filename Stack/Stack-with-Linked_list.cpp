306f4e4406548d31e0744f574b6b41fa30e74abc
Ahad Ali <ranaahad9095@gmail.com>
2025-02-23 21:45:05 -0800

    Practicing Stack with Linked_list...

---
+++ Stack/Stack-with-Linked_list.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+};
+
+class stack{
+public:
+    node* head = new node;
+    stack(){
+        head = NULL;
+    }
+    void push(int data){
+        node* new_node = new node;
+        new_node->data = data;
+        new_node->next = head;
+        head = new_node;
+    }
+    void pop(){
+        node* temp = head;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    void display(){
+        node* curr = head;
+        while (curr != NULL)
+        {
+            cout << curr->data << endl;
+            curr = curr->next;
+        }
+        cout << "--------------\n";
+    }
+    void peek(){
+        cout << head->data << endl;
+    }
+    bool isEmpty(){
+        if(head == NULL){
+            return true;
+        }
+        else
+        {
+            return false;
+            
+        }
+        
+    }
+};
+
+int main()
+{
+    bool var;
+    stack s1;
+    
+    s1.isEmpty();
+    var = s1.isEmpty();
+    cout << var << endl;
+    s1.push(9);
+    s1.push(50);
+    s1.pop();
+    s1.push(12);
+    s1.pop();
+    s1.push(77);
+    s1.push(0);
+    s1.push(3);
+    s1.pop();
+    s1.pop();
+    s1.display();
+    s1.peek();
+
+    cout << var << endl;
+    cout << "Everything is fine!" << endl;
+    return 0;
+}
