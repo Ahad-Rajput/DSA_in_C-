5d65536a71fe0f6fbec2723930c821ac1ac14054
Ahad Ali <ranaahad9095@gmail.com>
2025-03-19 23:27:04 -0700

    Completing the project idea..

--- project/Snake_game.cpp
+++ project/Snake_game.cpp
@@ -10,6 +10,9 @@ const int height = 20;
 
 int x, y, fruitX, fruitY, score;
 
+int tailX[100], tailY[100];
+int nTail;
+
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -56,8 +59,17 @@ void Draw(){
                 cout << "F";
             }
             else{
-
-                cout << " ";
+                bool print = false;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        print = true;
+                        cout << "o";
+                    }
+                }
+                if(!print)
+                    cout << " ";
             }
             if(j == width-1){
                 cout << "#";
@@ -98,6 +110,21 @@ void Input(){
     
 }
 void Logic(){
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    
     switch (dir)
     {
 
@@ -121,11 +148,19 @@ void Logic(){
     {
         gameOver = true;
     }
+    for (int i = 0; i < nTail; i++)
+    {
+        if(tailX[i] == x && tailY[i] == y){
+            gameOver = true;
+        }
+    }
+    
     if (x == fruitX && y == fruitY)
     {
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
+        nTail++;
     }
     
 }
