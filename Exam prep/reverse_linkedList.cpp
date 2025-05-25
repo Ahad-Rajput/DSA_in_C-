66f67333b41b5aba38bfda3cb2905be58b9fcda3
Ahad Ali <ranaahad9095@gmail.com>
2025-05-24 19:49:57 -0700

    Prepration for practical..

---
+++ Exam prep/reverse_linkedList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;    
+};
+
+class linked_list{
+    public:
+        node* head = new node;
+        linked_list(){
+            head = NULL;
+        }
+        void insertion(int data){
+            node* new_node = new node;
+            new_node->data = data;
+            new_node->next = head;
+            head = new_node;
+        }
+        void display(){
+            node* curr = head;
+            cout << "[ ";
+            while (curr != NULL)
+            {
+                cout << curr->data << " ";
+                curr = curr->next; 
+            }
+            cout << "]\n";
+        }
+        void reverse(){
+            node* prev_node = NULL;
+            node* curr_node, *next_node;
+            curr_node = next_node = head;
+            while (next_node != NULL)
+            {
+                next_node = next_node->next;
+                curr_node->next = prev_node;
+                prev_node = curr_node;
+                curr_node = next_node;
+            }
+            head = prev_node;
+        }
+};
+
+int main()
+{
+    linked_list l;
+    l.insertion(10);
+    l.insertion(20);
+    l.insertion(30);
+    l.insertion(40);
+    l.display();
+    l.reverse();
+    l.display();
+    cout << "Everything is fine!\n";
+    return 0;
+}
